d8ad029af3f37074dbbde7a38a96ce6defa1d634
kudinov-ihor <kudinov.igor98@gmail.com>
2023-05-07 23:09:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import { Drawer, Box, Typography } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 function App() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <MenuIcon onClick={() => setDrawerOpen(true)}></MenuIcon>
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <Box p={2} width="250px" textAlign="center" role="presentation">
+          <Link to="/">Main</Link>
+          <Link to="/about">About</Link>
+        </Box>
+      </Drawer>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
